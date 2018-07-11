38ccfb891c89a59c90ea562df01347cd47ace0a4
gichangA <gichanga@as-research.org>
2018-07-12 02:17:51 +0300

    OpenClover
    Refreshed Code Coverage

---
+++ src/com/declspec/gichanga/GcsGuiTest.java
@@ -0,0 +1,100 @@
+package com.declspec.gichanga;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GcsGuiTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetGui() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMain() {
+		GcsGui.main(null);
+	}
+
+	@Test
+	void testGcsGui() {
+		GcsGui gui = new GcsGui();
+		gui.setVisible(true);
+	}
+
+	@Test
+	void testActionPerformed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testStateChanged() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testLoadOptionsFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testProcessWindowEventWindowEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testProcessParameters() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testChooseDirectory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetProjectSource() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetMPLABDirectory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuildCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCanMakeBackup() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSbsTrackingSelectionEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
