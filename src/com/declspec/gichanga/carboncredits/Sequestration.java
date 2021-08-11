a768f24b09d82dcf0471f078a69aa886e8c5ff93
gichangA <gichanga@as-research.org>
2021-08-11 17:11:20 +0300

    Carbon monitoring
    toying with the idea of having large sets of data from which carbon credits can be  mined

---
+++ src/com/declspec/gichanga/carboncredits/Sequestration.java
@@ -0,0 +1,29 @@
+package com.declspec.gichanga.carboncredits;
+
+/**
+ * 
+ * @author moses gichanga
+ * 
+ * Brainstorming ideas along sequestration: Long term monitoring of vegetative ecosystems for
+ * 1. Threat Identification
+ * 2. Growth of vegetation to track carbon capture
+ * 3. Addition of a blockchain wallet for carbon credits for smallholder farmers
+ *
+ */
+public abstract class Sequestration {
+	public abstract void initialize();
+	abstract public void setVegetationType();
+	abstract protected Carbon getAreaCarbon() throws NoCarbonSetException;
+	
+}
+
+class Carbon{}
+class NoCarbonSetException extends Exception{
+
+	/**
+	 * SerialVersionUID for serialization
+	 * @author moses gichanga
+	 */
+	private static final long serialVersionUID = -2727630265450827217L;
+	
+}
